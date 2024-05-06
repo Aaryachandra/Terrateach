c7ce479d48261ade2987893fb39319d44f576ba9
Aaryachandra <aaryaachandra@gmail.com>
2024-05-07 00:01:11 +0530

    adding cors

--- annotate/app.js
+++ annotate/app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
+const cors = require("cors");
 const mongoose = require("mongoose");
+
+app.use(cors());
 
 //mongoose setup
 
